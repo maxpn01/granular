77f8681a9c2a97b246dc29c806b34a2327eb5757
maxpn01 <cypher.cr@protonmail.com>
2026-03-30 11:55:08 +0400

    comment out user defaults and add total progress logic

--- src/app/ui/user-profile/user-profile.ts
+++ src/app/ui/user-profile/user-profile.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, computed, inject } from "@angular/core";
+import { BoardService } from "../../core/services/board.service";
+import { TaskStatus } from "../../shared/models/task.model";
 
 @Component({
   selector: "app-user-profile",
@@ -6,4 +8,23 @@ import { Component } from "@angular/core";
   templateUrl: "./user-profile.html",
   styleUrl: "./user-profile.scss",
 })
-export class UserProfile {}
+export class UserProfile {
+  readonly boardService = inject(BoardService);
+
+  readonly totalTasks = computed(() => this.boardService.tasks().length);
+  readonly doneTasks = computed(
+    () =>
+      this.boardService
+        .tasks()
+        .filter((task) => task.status === TaskStatus.DONE).length,
+  );
+
+  readonly progressPercent = computed(() => {
+    const total = this.totalTasks();
+    const done = this.doneTasks();
+
+    if (total === 0) return 0;
+
+    return Math.round((done / total) * 100);
+  });
+}
